f53d301e9270c0d81c20323142f223bdfed49a7a
r.tereshchenko <r.tereschchenko@seclgroup.com>
2020-11-12 17:22:27 +0200

    done with the form section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(function () {
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false
-    })
+    });
+
+    $('select').styler();
 
 });
